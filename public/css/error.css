b7c8eb25991a16e9d3a28abd559c7e74a50338d9
yazilimvegelisim <motocelik23@gmail.com>
2024-01-16 11:57:43 +0300

    version 1.0.9

---
+++ public/css/error.css
@@ -0,0 +1,22 @@
+.error {
+   width: 100%;
+   height: calc(100vh - 50px);
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   position: relative;
+}
+
+.error i {
+   position: absolute;
+   font-size: 20rem;
+   z-index: 5;
+   color: rgb(255, 87, 87, 0.3);
+}
+
+.error span {
+   font-size: 5rem;
+   z-index: 10;
+   font-family: 'Josefin Sans', sans-serif;
+   font-weight: 700;
+}
